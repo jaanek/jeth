5055fbb9549c47665404b7606ffc841188f20dd7
Jaanek Oja <jaanekoja@gmail.com>
2021-09-14 11:28:21 +0300

    expose general Send method

--- eth/abi.go
+++ eth/abi.go
@@ -264,3 +264,30 @@ func Deploy(term ui.Screen, endpoint rpc.Endpoint, from common.Address, bin []by
 	}
 	return hash, receipt, nil
 }
+
+func Send(term ui.Screen, endpoint rpc.Endpoint, from common.Address, to common.Address, value *uint256.Int, data []byte, waitTime time.Duration, txSigner TxSigner) (string, *TxReceipt, error) {
+	// estimate params, gas etc.
+	params, err := GetTransactionParams(term, endpoint, from, &to, value, data, Latest)
+	if err != nil {
+		return "", nil, fmt.Errorf("Error while getting tx params for a method call: %w", err)
+	}
+
+	// get signed tx and send it
+	encoded, err := txSigner.GetSignedRawTx(*params.ChainId, *params.TxCount, from, &to, value, data, *params.Gas, params.GasPrice, params.GasTip, params.GasPrice)
+	hash, err := SendTransaction(term, endpoint, encoded)
+	if err != nil {
+		return "", nil, fmt.Errorf("Failed to send tx: %w", err)
+	}
+	// m.term.Print(fmt.Sprintf("Tx hash: %s", hash))
+
+	// wait for tx receipt
+	if waitTime < 0 {
+		waitTime = ReceiptWaitTime
+	}
+	c, _ := context.WithTimeout(context.Background(), waitTime)
+	receipt, err := WaitTransactionReceipt(c, term, endpoint, hash)
+	if err != nil {
+		return hash, nil, err
+	}
+	return hash, receipt, nil
+}
